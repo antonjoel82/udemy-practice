ab2771a064f7d37f6a37d45e47da351150cf3c6a
Joel Anton <anton.joel82@gmail.com>
2019-04-08 21:13:51 +0100

    Completed ES8 Async Await exercises

---
+++ Section20_Async/exerciseAsyncAwait.js
@@ -0,0 +1,50 @@
+// Solve the below problems:
+
+// #1) Convert the below promise into async await
+fetch('https://swapi.co/api/starships/9/')
+  .then(response => response.json())
+  .then(console.log)
+
+async function f1() {
+  const fet = await fetch('https://swapi.co/api/starships/9/');
+  const resp = await fet.json();
+  console.log(resp);
+}
+
+
+// #2) ADVANCED: Update the function below from the video to also have
+// async await for this line: fetch(url).then(resp => resp.json())
+// So there shouldn't be any .then() calls anymore!
+// Don't get discouraged... this is a really tough one...
+const urls = [
+  'https://jsonplaceholder.typicode.com/users',
+  'https://jsonplaceholder.typicode.com/posts',
+  'https://jsonplaceholder.typicode.com/albums'
+]
+
+const getData = async function() {
+  const [ users, posts, albums ] = await Promise.all(urls.map(url =>
+      fetch(url).then(resp => resp.json())
+  ));
+  console.log('users', users);
+  console.log('posta', posts);
+  console.log('albums', albums);
+}
+
+async function f2 () {
+  try {
+    const [ users, posts, albums ] = await Promise.all(urls.map(async function(url) {
+        const fet = await fetch(url);
+        return fet.json();
+    }));
+    console.log('users', users);
+    console.log('posts', posts);
+    console.log('albums', albums);
+  } catch (err) {
+    console.log(`Surry... ${err}`);
+  }
+}
+
+// #3) Add a try catch block to the #2 solution in order to catch any errors.
+// Now chnage one of the urls so you console.log your error with 'ooooooops'
+//See above
