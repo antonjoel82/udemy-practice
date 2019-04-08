64ccef3c7ae4a1d856229dcca50161d571ee645a
Joel Anton <anton.joel82@gmail.com>
2019-04-08 16:22:53 +0100

    Finished RoboFriends app. Restructured directories.

--- Section19_React/robofriends/src/components/Card.js
+++ Section19_React/robofriends/src/components/Card.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Card = ({name, email, username, id}) => {
     return (
-        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img alt="robots" src={`https://robohash.org/${username}${id}?200x200`} />
+        <div className="tc bg-light-green dib br3 pa2 ma2 grow bw2 shadow-5">
+            <img alt="robots" src={`https://robohash.org/${username}${id}?140x140`} />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
