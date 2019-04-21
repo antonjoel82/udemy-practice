a63c156ede292f9e173cac16c53e615254741b0a
Joel Anton <anton.joel82@gmail.com>
2019-04-21 19:50:49 +0100

    Did AdventOfCode exercise

--- Section24_NodeExpress/FileSystemModule/index.js
+++ Section24_NodeExpress/FileSystemModule/index.js
@@ -1,11 +1,30 @@
 const fs = require("fs");
 
-fs.readFile(`./HelloWorld.txt`, (err, data) => {
+const open = "(".charCodeAt(0);
+const closed = ")".charCodeAt(0);
+fs.readFile(`./input.txt`, (err, data) => {
+	console.time("solution");
 	if (err) {
 		console.log("Read Error!");
 		return;
 	}
-	console.log(data.toString());
+
+	let posOfBasement = -1;
+	const finFloor = data.reduce((counter, c) => {
+		return c === open
+			? ++counter
+			: c === closed
+				? --counter
+				: counter -= 100
+
+		// if (counter < 0 && posOfBasement === -1) {
+		// 	posOfBasement = ndx + 1;
+		// }
+	}, 0)
+
+	console.timeEnd("solution");
+	console.log(`Ending floor: ${finFloor}`);
+	console.log(`Entered basement on Instruction #${posOfBasement}`);
 });
 
 // fs.writeFile(`./HelloWorld.txt`, "Test write 9.", (err) => {
